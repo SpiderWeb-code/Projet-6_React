39b2943ade0485302b6b96f236c9430030439009
SpiderWeb-code <125570323+SpiderWeb-code@users.noreply.github.com>
2023-12-29 15:58:06 +0100

    Creation du composant Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "./Header.css"
+import Logo from "../../Data/logo.png"
 
-function App() {
+function Header() {
+  //STATE
+
+  //COMPORTEMENT
+
+  //AFFICHAGE
   return (
-    <div>
-
+    <div id="container_header">
+      <img src={Logo} alt="Logo"></img>
+      <nav id="nav_header">
+        <p id="p_accueil"><strong>Accueil</strong></p>
+        <p><strong>A Propos</strong></p>
+      </nav>
     </div>
   );
 }
 
-export default App;
+export default Header;

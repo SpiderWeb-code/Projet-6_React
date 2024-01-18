84caf1a7699905b0bf7a32695d5d3d273e994207
SpiderWeb-code <125570323+SpiderWeb-code@users.noreply.github.com>
2024-01-18 13:30:17 +0100

    Changement de curseur sur les articles

--- src/Components/Page-Accueil/Main/article.jsx
+++ src/Components/Page-Accueil/Main/article.jsx
@@ -3,10 +3,12 @@ import React from "react";
         //STATE
     
         //COMPORTEMENT
-    
+        const handleClick = () =>{
+            console.log("Hello World");
+        }
         //RENDER
         return(
-            <article className="article" style={{height:"435px", maxWidth:"435px", width:"100%", border:"1px solid black", borderRadius:"10px", margin:"2%", display:"flex", alignItems:"end"}}>
+            <article className="article" onClick={handleClick} style={{height:"435px", maxWidth:"435px", width:"100%", border:"1px solid black", borderRadius:"10px", margin:"2%", display:"flex", alignItems:"end", cursor:"pointer"}}>
             <h2 style={{margin:"0px 0px 15px 15px", position:"absolute", maxWidth:"20%", color:"white"}}>{props.title}</h2>
             <img src={props.cover} alt="images-articles" style={{height:"435px", maxWidth:"435px", width:"100%", objectFit:"cover"}}></img>
             </article>

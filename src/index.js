6d4ea5dcb5abfc60d01bfbc134cb681ac0b647f4
SpiderWeb-code <125570323+SpiderWeb-code@users.noreply.github.com>
2024-01-19 13:22:48 +0100

    Amélioration du Rooter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Rooter from './rooter';
-import './index.css';
-import Data from '../src/Data/data.json'
-import Logo from './Components/Page-Accueil/Header/logo';
-import Nav from './Components/Page-Accueil/Header/navigation';
-import SectionImage from './Components/Page-Accueil/Main/section-image';
-import Footer from '../src/Components/Page-Accueil/Footer/footer';
-import Article from './Components/Page-Accueil/Main/article';
 
 //STATE
 
@@ -18,18 +11,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Rooter />
-    <header>
-    <Logo />
-    <Nav />
-    </header>
-    <SectionImage />
-    <section className='container-article'>
-      {Data.map(({title, id, cover}) => (
-      //<Article key={id} title={title} cover={cover}/>
-        <Article key={id} title={title} cover={cover}/>
-      ))}
-    </section>
-    <Footer />
   </React.StrictMode>
 );
 

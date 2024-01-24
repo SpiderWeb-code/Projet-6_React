f37895b3c968ac9e566d6b91be0bba9bd00e5da9
SpiderWeb-code <125570323+SpiderWeb-code@users.noreply.github.com>
2024-01-24 18:14:23 +0100

    Ajouts des tags dans la page Articles

--- src/Components/Localisation/localisation.jsx
+++ src/Components/Localisation/localisation.jsx
@@ -6,9 +6,18 @@ function Localisation(props){
         <div id="first-container">
             <h2>{props.title}</h2>
             <p id="location">{props.location}</p>
+            <div id="container-tags">
+            <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            </ul>
+            <div id="button-location">
             <button>{props.tags[0]}</button>
             <button>{props.tags[1]}</button>
             <button>{props.tags[2]}</button>
+            </div>
+            </div>
             </div>
     )
 }

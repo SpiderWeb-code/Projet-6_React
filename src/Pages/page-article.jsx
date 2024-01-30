66754292f6f47bd434811c72533ec4d677f81917
SpiderWeb-code <125570323+SpiderWeb-code@users.noreply.github.com>
2024-01-30 17:54:24 +0100

    Clique droit fonctionnelle

--- src/Pages/page-article.jsx
+++ src/Pages/page-article.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import Logo from "../Components/Logo/logo";
-import Nav from "../Components/Navigation/navigation";
-import Banner from "../Components/Banner/banner";
-import Article from "../Components/Article/article";
-import { useParams } from "react-router-dom";
-import Data from "../Components/Data/data.json";
-import "../Components/Article/article.css";
+import React, { useState } from 'react';
+import Logo from '../Components/Logo/logo';
+import Nav from '../Components/Navigation/navigation';
+import Banner from '../Components/Banner/banner';
+import Article from '../Components/Article/article';
+import { useParams } from 'react-router-dom';
+import Data from '../Components/Data/data.json';
+import '../Components/Article/article.css';
 import Profil from '../Components/Profil/Profil';
 import Localisation from '../Components/Localisation/localisation';
 import Rating from '../Components/Rating/rating';
@@ -13,37 +13,24 @@ import OpenLabel from '../Components/Open label/open-label';
 import { titleLabelDescription } from '../Components/Open label/open-label';
 import { titleLabelEquipements } from '../Components/Open label/open-label';
 import Switch from '../Components/Switch/switch';
-import { useState } from 'react';
 
 function ArticlePage() {
   let params = useParams();
   let paramsId = params.id;
 
   // Filtrer les éléments dans le tableau Data
-  const [cover, setCover] = useState("");
+  const [coverIndex, setCoverIndex] = useState(0);
 
   // Filtrer les éléments dans le tableau Data
   const filteredData = Data.filter((props) => props.id === paramsId);
 
   const handleSwitchLeft = () => {
-    // Mettre à jour la valeur de cover en fonction de la logique souhaitée pour la flèche gauche
-    //Récupére le premier lien en fonction de l'id de L'URL
-    let index = 0
-    index++
-    setCover(
-      filteredData.map(props => props.pictures[index])
-      )
+    setCoverIndex((prevIndex) => (prevIndex - 1 + filteredData.length) % filteredData[0]?.pictures?.length);
   };
 
   const handleSwitchRight = () => {
-    // Mettre à jour la valeur de cover en fonction de la logique souhaitée pour la flèche droite
-    let index = 5
-    index--
-    setCover(
-      filteredData.map(props => props.pictures[index])
-    );
+    setCoverIndex((prevIndex) => (prevIndex + 1) % filteredData[0]?.pictures?.length);
   };
-
 
   return (
     <main id="main_page-article">
@@ -53,35 +40,32 @@ function ArticlePage() {
       </header>
       <div className="container-article-2">
         {/* Afficher les articles filtrés */}
-        {filteredData.map(
-          ({ id, title, cover: articleCover, location, host, tags, rating, description, equipments }) => (
-            <React.Fragment key={id}>
-              <Switch onClickLeft={handleSwitchLeft} onClickRight={handleSwitchRight} />
-              {/* Utiliser la variable d'état cover ici */}
-              <Article cover={cover || articleCover || "valeur_par_defaut_pour_cover"} />
-              <section id="second-container">
-                <Localisation title={title} location={location} tags={tags} />
-                <div id="container-profil">
-                  <Profil name={host.name} picture={host.picture} />
-                  <Rating rating={rating} />
-                </div>
-              </section>
-              <section id="container-labels">
-                <OpenLabel label={titleLabelDescription} description={<p>{description}</p>} />
-                <OpenLabel
-                  label={titleLabelEquipements}
-                  equipements={
-                    <ul>
-                      {equipments.map((item, index) => (
-                        <li key={index}>{item}</li>
-                      ))}
-                    </ul>
-                  }
-                />
-              </section>
-            </React.Fragment>
-          )
-        )}
+        {filteredData.map(({ id, title, cover: articleCover, location, host, tags, rating, description, equipments }) => (
+          <React.Fragment key={id}>
+            <Switch onClickLeft={handleSwitchLeft} onClickRight={handleSwitchRight} />
+            <Article cover={filteredData[0]?.pictures?.[coverIndex] || articleCover || 'valeur_par_defaut_pour_cover'} />
+            <section id="second-container">
+              <Localisation title={title} location={location} tags={tags} />
+              <div id="container-profil">
+                <Profil name={host.name} picture={host.picture} />
+                <Rating rating={rating} />
+              </div>
+            </section>
+            <section id="container-labels">
+              <OpenLabel label={titleLabelDescription} description={<p>{description}</p>} />
+              <OpenLabel
+                label={titleLabelEquipements}
+                equipements={
+                  <ul>
+                    {equipments.map((item, index) => (
+                      <li key={index}>{item}</li>
+                    ))}
+                  </ul>
+                }
+              />
+            </section>
+          </React.Fragment>
+        ))}
       </div>
       <Banner />
     </main>

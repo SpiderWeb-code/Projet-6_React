6f5c9381f443bb5d706d18bc86b9badbfc758928
SpiderWeb-code <125570323+SpiderWeb-code@users.noreply.github.com>
2024-02-05 16:32:12 +0100

    Changement du footer

--- src/Components/Banner/banner.jsx
+++ src/Components/Banner/banner.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import FooterImage from "../Data/Footer.webp";
+import FooterImageMobil from "../Data/Kasa-image-mobil-banner.webp"
 import "../Banner/banner.css";
 function Banner(){
     return(
     <div>
-        <img id="banner" src={FooterImage} alt="bas de page avec dessus le logo du site de Kasa"></img>
+        <picture>
+            <source srcSet={FooterImageMobil} media="(max-width: 425px)" alt="bas de page avec dessus le logo du site de Kasa"></source>
+            <img src={FooterImage} alt="bas de page avec dessus le logo du site de Kasa" id="banner"></img>
+        </picture>
     </div>
     )
 }

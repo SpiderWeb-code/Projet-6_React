c19249f17b8060c16106e960c3bd048e0ce4a1d2
SpiderWeb-code <125570323+SpiderWeb-code@users.noreply.github.com>
2024-02-10 23:40:44 +0100

    Changement image Footer Par Texte

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import FooterImage from "../Data/Footer.webp";
-import FooterImageMobil from "../Data/Kasa-image-mobil-banner.webp"
+import FooterImage from "../Data/logo-footer.webp";
 import "./footer.css";
 function Footer(){
     return(
-    <div>
+    <div id="banner">
         <picture>
-            <source srcSet={FooterImageMobil} media="(max-width: 425px)" alt="bas de page avec dessus le logo du site de Kasa"></source>
-            <img src={FooterImage} alt="bas de page avec dessus le logo du site de Kasa" id="banner"></img>
+            <source srcSet={FooterImage} media="(max-width: 425px)" alt="bas de page avec dessus le logo du site de Kasa"></source>
+            <img src={FooterImage} alt="bas de page avec dessus le logo du site de Kasa"></img>
         </picture>
+        <strong>© 2020 Kasa. All rights reserved</strong>
     </div>
     )
 }

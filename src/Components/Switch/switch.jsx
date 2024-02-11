d1743aed0329dfb2421e7adfc6235a455cb96916
SpiderWeb-code <125570323+SpiderWeb-code@users.noreply.github.com>
2024-02-11 17:20:20 +0100

    Correction responsive fléche + number Article

--- src/Components/Switch/switch.jsx
+++ src/Components/Switch/switch.jsx
@@ -5,14 +5,19 @@ import ArrowRight from "../Data/angle-right-solid.svg";
 
 function Switch({ onClickLeft, onClickRight, coverIndex, maxCoverIndex }){
     return(
+        <div id="container-switch-buttons">
         <div id="switch-buttons">
             <div className="div-button" onClick={onClickLeft}><img src={ArrowLeft} alt="arrow left"></img></div>
             <div className="div-button" onClick={onClickRight}><img src={ArrowRight} alt="arrow right"></img></div>
+            
+        </div>
+        <div id="container-ul-switch">
             <ul>
                 <li>{coverIndex + 1}</li>
                 <li>/</li>
                 <li>{maxCoverIndex + 1}</li>
             </ul>
+            </div>
         </div>
     )
 };

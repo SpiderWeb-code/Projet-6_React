bfd69170e1e82503d3d41b8d7dd73c1784d511c2
SpiderWeb-code <125570323+SpiderWeb-code@users.noreply.github.com>
2024-02-12 10:57:33 +0100

    Supprime le Switch si maxIndex > 1 = une image

--- src/Components/Switch/switch.jsx
+++ src/Components/Switch/switch.jsx
@@ -6,10 +6,12 @@ import ArrowRight from "../Data/angle-right-solid.svg";
 function Switch({ onClickLeft, onClickRight, coverIndex, maxCoverIndex }){
     return(
         <div id="container-switch-buttons">
+            {maxCoverIndex > 1 &&
+            <>
         <div id="switch-buttons">
             <div className="div-button" onClick={onClickLeft}><img src={ArrowLeft} alt="arrow left"></img></div>
             <div className="div-button" onClick={onClickRight}><img src={ArrowRight} alt="arrow right"></img></div>
-            
+
         </div>
         <div id="container-ul-switch">
             <ul>
@@ -18,6 +20,8 @@ function Switch({ onClickLeft, onClickRight, coverIndex, maxCoverIndex }){
                 <li>{maxCoverIndex + 1}</li>
             </ul>
             </div>
+            </>
+}
         </div>
     )
 };
